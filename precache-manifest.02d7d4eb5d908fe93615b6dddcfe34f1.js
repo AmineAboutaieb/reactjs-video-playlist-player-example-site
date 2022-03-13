d068edd141e585fb3baf359a98efefaad6a5efe5
AmineAboutaieb <amine.aboutaieb@gmail.com>
2022-03-13 14:08:40 +0100

    first commit

---
+++ precache-manifest.02d7d4eb5d908fe93615b6dddcfe34f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4f42a67154e2d43384754e6807989c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ea772bb952bf95430f7",
+    "url": "./static/css/main.f148f442.chunk.css"
+  },
+  {
+    "revision": "6c6c9a807596fda27dcf",
+    "url": "./static/js/2.23bc3444.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.23bc3444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea772bb952bf95430f7",
+    "url": "./static/js/main.6093b1f1.chunk.js"
+  },
+  {
+    "revision": "868927d8d2e8b2c1dd06",
+    "url": "./static/js/runtime-main.ff3ee701.js"
+  },
+  {
+    "revision": "bd8b9512f198eb0e80b5d473037d41d6",
+    "url": "./static/media/video-placeholder.bd8b9512.jpg"
+  }
+]);
